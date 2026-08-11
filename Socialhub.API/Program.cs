257647cc12bd1f31b193b555e551eaffd76b9017
emilecode <williamsemile1234567@gmail.com>
2026-08-11 16:51:30 +0200

    fix self made bugs such a pain!

--- Socialhub.API/Program.cs
+++ Socialhub.API/Program.cs
@@ -5,16 +5,17 @@ using Socialhub.API.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+string connectionString = "PostgresConnection";
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>{
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
     options.UseNpgsql(
-        builder.Configuration.GetConnectionString("PostgresConnection")
+        builder.Configuration.GetConnectionString(connectionString)
     );
 });
-builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Socialhub.API/Services/UserService.cs
+++ Socialhub.API/Services/UserService.cs
@@ -5,35 +5,36 @@ using Socialhub.API.Entities;
 using Socialhub.API.DTOs;
 using BCryptNet = BCrypt.Net.BCrypt;
 namespace Socialhub.API.Services;
+
 public class UserService : IUserService
 {
     private readonly ApplicationDbContext _context;
-    
+
     public UserService(ApplicationDbContext context)
     {
         _context = context;
-        
+
     }
-    
+
     public async Task<UserResponseDto?> CreateAsync(UserRequestDto dto)
     {
-        if(dto is null)
+        if (dto is null)
         {
             return null;
         }
         bool emailExists = await _context.Users.AnyAsync(u => u.Email == dto.Email);
-        if(emailExists)
+        if (emailExists)
         {
             return null;
-        }    
+        }
         string secureHash = BCryptNet.HashPassword(dto.Password);
         var newUser = new User
         {
-          Username = dto.Username,
-          Email = dto.Email,
-          PasswordHash =  secureHash 
+            Username = dto.Username,
+            Email = dto.Email,
+            PasswordHash = secureHash
         };
-        
+
         _context.Users.Add(newUser);
         await _context.SaveChangesAsync();
         return new UserResponseDto
@@ -43,7 +44,7 @@ public class UserService : IUserService
             newUser.Email
         );
     }
-    
-     
-    
+
+
+
 }
